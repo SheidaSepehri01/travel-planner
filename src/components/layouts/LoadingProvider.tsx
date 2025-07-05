91d8e55faaa4f70777a8208704f7eed7e985d516
sheidasepehri01 <sheidasepehri01@gmail.com>
2025-07-05 17:03:52 +0330

    loading style edit

--- src/components/layouts/LoadingProvider.tsx
+++ src/components/layouts/LoadingProvider.tsx
@@ -1,6 +1,7 @@
 // components/LoadingProvider.tsx
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export const LoadingProvider = ({
@@ -18,7 +19,14 @@ export const LoadingProvider = ({
 
   if (isLoading) {
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-amber-200 w-screen h-screen">
+      <div className="fixed inset-0 z-50 flex flex-col items-center justify-center  w-screen h-screen">
+        <Image
+          src={"/assets/images/loading.gif"}
+          alt="loading icon"
+          width={100}
+          height={100}
+          unoptimized={true}
+        />
         <h1 className="text-2xl font-bold animate-pulse text-black">
           Loading...
         </h1>
